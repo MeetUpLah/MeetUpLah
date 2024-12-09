43cfad27c40ef72b2821d8163d48908946d46314
Jeremy <e1155908@u.nus.edu>
2024-12-09 19:16:22 +0800

    Add some text input to test firestore

--- app/screens/(tabs)/trips/components/addTrip.tsx
+++ app/screens/(tabs)/trips/components/addTrip.tsx
@@ -1,34 +1,46 @@
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, TextInput} from 'react-native';
 import {useRouter} from "expo-router";
 import firestore from "@react-native-firebase/firestore"
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import auth from "@react-native-firebase/auth";
-
-
 
 export default function AddTripScreen() {
 
     const router = useRouter();
 
-    const getData = async () => {
-        try {
-            const tripDocument =  await firestore()
-                .collection('trips')
-                .doc('Me3FUeflX1yOAHsY1kR2').get();
+    const [name, setName] = useState('');
+    const [accommodation, setAccommodation] = useState('');
+    const [flight, setFlight] = useState('');
 
-            console.log(tripDocument.data());
-        } catch (error) {
-            console.error('Error fetching document:', error);
+    const currentUser = auth().currentUser;
+
+    // Add trip by user id
+    const handleAddTrip = () => {
+        if (!currentUser) {
+            console.error("No user logged in");
+            return;
         }
+
+        firestore()
+            .collection('trips')
+            .doc(currentUser.uid)
+            .collection('userTrips') // Subcollection for the user's trips
+            .add({ // Add a new trip document
+                Name: name,
+                Accommodation: accommodation,
+                Flight: flight,
+                createdAt: firestore.FieldValue.serverTimestamp(),
+            })
+            .then(() => {
+                console.log('Trip added!');
+                setName('');
+                setAccommodation('');
+                setFlight('');
+            })
+            .catch((error) => {
+                console.error('Error adding trip:', error);
+            });
     };
-
-
-    useEffect(() => {
-        getData();
-
-        console.log(auth().currentUser);
-        console.log(auth().currentUser?.uid);
-    }, []);
 
 
     return (
@@ -36,6 +48,33 @@ export default function AddTripScreen() {
             <Text>Add Trip Page</Text>
             <TouchableOpacity onPress={() => router.back()}>
                 <Text>Go back</Text>
+            </TouchableOpacity>
+
+            <TextInput
+                style={styles.input}
+                placeholder='Name'
+                value={name}
+                onChangeText={setName}
+                keyboardType='email-address'
+            />
+
+            <TextInput
+                style={styles.input}
+                placeholder='Accommodation'
+                value={accommodation}
+                onChangeText={setAccommodation}
+                keyboardType='email-address'
+            />
+            <TextInput
+                style={styles.input}
+                placeholder='Flight'
+                value={flight}
+                onChangeText={setFlight}
+                keyboardType='email-address'
+            />
+
+            <TouchableOpacity onPress={handleAddTrip}>
+                <Text>Add</Text>
             </TouchableOpacity>
 
         </View>
@@ -47,5 +86,14 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-    }
+    },
+    input: {
+        marginVertical: 4,
+        height: 50,
+        borderRadius: 4,
+        borderWidth: 1,
+        padding: 10,
+        backgroundColor: `#fff`,
+        width: 300
+    },
 });
