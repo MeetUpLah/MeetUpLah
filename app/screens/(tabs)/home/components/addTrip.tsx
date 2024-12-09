0c52d49bba2366b9831c523a52c740aca9e4fc28
Jeremy <e1155908@u.nus.edu>
2024-12-09 16:31:44 +0800

    Connect app to Firestore

--- app/screens/(tabs)/home/components/addTrip.tsx
+++ app/screens/(tabs)/home/components/addTrip.tsx
@@ -1,12 +1,34 @@
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import {useRoute} from "@react-navigation/core";
 import {useRouter} from "expo-router";
+import firestore from "@react-native-firebase/firestore"
+import {useEffect} from "react";
+import auth from "@react-native-firebase/auth";
+
 
 
 export default function AddTripScreen() {
 
-
     const router = useRouter();
+
+    const getData = async () => {
+        try {
+            const tripDocument =  await firestore()
+                .collection('trips')
+                .doc('Me3FUeflX1yOAHsY1kR2').get();
+
+            console.log(tripDocument.data());
+        } catch (error) {
+            console.error('Error fetching document:', error);
+        }
+    };
+
+
+    useEffect(() => {
+        getData();
+
+        console.log(auth().currentUser);
+        console.log(auth().currentUser?.uid);
+    }, []);
 
 
     return (
